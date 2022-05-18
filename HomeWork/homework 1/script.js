662f7b2dc3500addcf038ab267dc433105d6a7b0
AniFuryYT <69161293+AniFuryYT@users.noreply.github.com>
2022-05-18 12:06:26 +0200

    Delete HomeWork directory

--- HomeWork/homework 1/script.js
+++
@@ -1,8 +0,0 @@
-alert('Hello!');
-let personName = prompt('What is your name?');
-alert('Nice to meet you, ' + personName);
-
-let thisYear = 2022;
-let personAge = prompt('How old are you?');
-let yearOfBirth = thisYear - personAge;
-alert('You were born in ' + yearOfBirth);
