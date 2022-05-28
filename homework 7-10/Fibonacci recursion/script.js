52797e90e6b3d0e019fe8625a3b7f6f7e07a9484
Vladyslav <69161293+AniFuryYT@users.noreply.github.com>
2022-05-28 17:03:24 +0300

    Update script.js

--- homework 7-10/Fibonacci recursion/script.js
+++ homework 7-10/Fibonacci recursion/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
 function fib(n) {
-	if (n == 1 || n == 2) {
-		return 1;
-	} else {
-		return fib(n - 2) + fib(n - 1);
-	}
+  if (n == 1 || n == 2) {
+    return 1;
+} else {
+  return fib(n - 2) + fib(n - 1);
+ }
 }
 console.log(fib(15));
