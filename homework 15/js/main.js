c90ec8c940ff162cdd85b3734b3d8294f6c529a6
Vladyslav <animew1243@gmail.com>
2022-06-25 19:41:57 +0300

    commit

---
+++ homework 15/js/main.js
@@ -0,0 +1,30 @@
+(() => {
+	'use strict';
+	const calc = defaultNum => {
+		return {
+			add: function add(secNum) {
+				return console.log(defaultNum + secNum);
+			},
+			sub: function sub(secNum) {
+				return console.log(defaultNum - secNum);
+			},
+			div: function div(secNum) {
+				return console.log(defaultNum / secNum);
+			},
+			mul: function mul(secNum) {
+				return console.log(defaultNum * secNum);
+			},
+			getResult: function getResult() {
+				return console.log(defaultNum);
+			},
+		};
+	};
+
+	const first = calc(4);
+	first.add(33);
+	first.sub(12);
+	first.div(2);
+	first.mul(4);
+
+	first.getResult();
+})();
