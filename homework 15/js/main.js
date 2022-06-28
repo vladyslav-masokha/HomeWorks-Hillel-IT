049d822a3f8c7baf4d19ffc3043a128ec9812886
Vladyslav <69161293+vladyslav-masokha@users.noreply.github.com>
2022-06-28 18:28:36 +0100

    commit

--- homework 15/js/main.js
+++ homework 15/js/main.js
@@ -3,16 +3,16 @@
 	const calc = defaultNum => {
 		return {
 			add: function add(secNum) {
-				return (defaultNum += secNum), console.log(defaultNum);
+				return console.log((defaultNum += secNum));
 			},
 			sub: function sub(secNum) {
-				return (defaultNum -= secNum), console.log(defaultNum);
+				return console.log((defaultNum -= secNum));
 			},
 			div: function div(secNum) {
-				return (defaultNum /= secNum), console.log(defaultNum);
+				return console.log((defaultNum /= secNum));
 			},
 			mul: function mul(secNum) {
-				return (defaultNum *= secNum), console.log(defaultNum);
+				return console.log((defaultNum *= secNum));
 			},
 			getResult: function getResult() {
 				return console.log(defaultNum);
@@ -24,7 +24,7 @@
 	first.add(33);
 	first.sub(12);
 	first.div(2);
-	first.mul(5);
+	first.mul(4);
 
 	first.getResult();
 })();
