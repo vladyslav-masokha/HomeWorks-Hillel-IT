c8c6e051ed95ba6b05d09bbcdb15c72adf377851
Vladyslav <69161293+vladyslav-masokha@users.noreply.github.com>
2022-07-06 09:26:19 +0300

    commit

--- homework 16/js/main.js
+++ homework 16/js/main.js
@@ -1,110 +1,99 @@
-function Hamburger(toppings) {
-	this.SIZE_SMALL = {
-		price: 50,
-		collories: 20,
-	};
-
-	this.SIZE_MEDIUM = {
-		price: 75,
-		collories: 30,
-	};
-
-	this.SIZE_BIG = {
-		price: 100,
-		collories: 40,
-	};
-
-	toppings = {
-		TOPPING_MAYO: {
-			updatePriceSmall: (this.SIZE_SMALL.price += 20),
-			updatePriceMedium: (this.SIZE_MEDIUM.price += 20),
-			updatePriceBig: (this.SIZE_BIG.price += 20),
-
-			updateColloriesSmall: (this.SIZE_SMALL.collories += 5),
-			updateColloriesMedium: (this.SIZE_MEDIUM.collories += 5),
-			updateColloriesBig: (this.SIZE_BIG.collories += 5),
-		},
-
-		TOPPING_CHEESE: {
-			updatePriceSmall: (this.SIZE_SMALL.price += 10),
-			updatePriceMedium: (this.SIZE_MEDIUM.price += 10),
-			updatePriceBig: (this.SIZE_BIG.price += 10),
-
-			updateColloriesSmall: (this.SIZE_SMALL.collories += 20),
-			updateColloriesMedium: (this.SIZE_MEDIUM.collories += 20),
-			updateColloriesBig: (this.SIZE_BIG.collories += 20),
-		},
-
-		TOPPING_SALAD: {
-			updatePriceSmall: (this.SIZE_SMALL.price += 20),
-			updatePriceMedium: (this.SIZE_MEDIUM.price += 20),
-			updatePriceBig: (this.SIZE_BIG.price += 20),
-
-			updateColloriesSmall: (this.SIZE_SMALL.collories += 5),
-			updateColloriesMedium: (this.SIZE_MEDIUM.collories += 5),
-			updateColloriesBig: (this.SIZE_BIG.collories += 5),
-		},
-
-		TOPPING_SEASONING: {
-			updatePriceSmall: (this.SIZE_SMALL.price += 15),
-			updatePriceMedium: (this.SIZE_MEDIUM.price += 15),
-			updatePriceBig: (this.SIZE_BIG.price += 15),
-
-			updateColloriesSmall: (this.SIZE_SMALL.collories += 0),
-			updateColloriesMedium: (this.SIZE_MEDIUM.collories += 0),
-			updateColloriesBig: (this.SIZE_BIG.collories += 0),
-		},
-
-		TOPPING_POTATO: {
-			updatePriceSmall: (this.SIZE_SMALL.price += 15),
-			updatePriceMedium: (this.SIZE_MEDIUM.price += 15),
-			updatePriceBig: (this.SIZE_BIG.price += 15),
-
-			updateColloriesSmall: (this.SIZE_SMALL.collories += 10),
-			updateColloriesMedium: (this.SIZE_MEDIUM.collories += 10),
-			updateColloriesBig: (this.SIZE_BIG.collories += 10),
-		},
-	};
-
-	this.addTopping = () => {
-		return toppings;
-	};
+function Burger(size, toppings = []) {
+	this.size = size;
+	this.toppings = toppings;
 
 	this.getPrice = () => {
-		return {
-			SIZE_SMALL: this.SIZE_SMALL.price,
-			SIZE_MEDIUM: this.SIZE_MEDIUM.price,
-			SIZE_BIG: this.SIZE_BIG.price,
-		};
+		return this.toppings.reduce(
+			(price, topping) => price + topping.price,
+			this.size.price
+		);
 	};
 
 	this.getCallories = () => {
-		return {
-			SIZE_SMALL: this.SIZE_SMALL.collories,
-			SIZE_MEDIUM: this.SIZE_MEDIUM.collories,
-			SIZE_BIG: this.SIZE_BIG.collories,
-		};
+		return this.toppings.reduce(
+			(kcal, topping) => kcal + topping.kcal,
+			this.size.kcal
+		);
+	};
+
+	this.burgerInfo = () => {
+		console.log(`
+-----------------
+BURGER SIZE: ${this.size.name}
+PRICE: $${this.getPrice()}
+CALLORIES: ${this.getCallories()} kcal
+TOPPINGS: ${toppings.map(e => e.name).join(', ')}
+-----------------
+
+`);
+	};
+
+	this.addTopping = topping => {
+		this.toppings.push(topping);
 	};
 }
 
-const hamburger = new Hamburger();
-// Small burger
-console.group('Small burger');
-hamburger.addTopping().TOPPING_MAYO;
-console.log('Price with sauce: ' + hamburger.getPrice().SIZE_SMALL);
-console.log('Callories with sauce: ' + hamburger.getCallories().SIZE_SMALL);
-console.groupEnd();
+Burger.BURGER_SIZE = {
+	SMALL: {
+		name: 'Small',
+		price: 50,
+		kcal: 20,
+	},
+	MEDIUM: {
+		name: 'Medium',
+		price: 75,
+		kcal: 30,
+	},
+	BIG: {
+		name: 'Big',
+		price: 100,
+		kcal: 40,
+	},
+};
 
-// Medium burger
-console.group('Medium burger');
-hamburger.addTopping().TOPPING_MAYO.updatePriceMedium;
-console.log('Price with sauce: ' + hamburger.getPrice().SIZE_MEDIUM);
-console.log('Callories with sauce: ' + hamburger.getCallories().SIZE_MEDIUM);
-console.groupEnd();
+Burger.TOPPINGS = {
+	CHEESE: {
+		name: 'CHEESE',
+		price: 10,
+		kcal: 20,
+	},
+	SALAD: {
+		name: 'SALAD',
+		price: 20,
+		kcal: 5,
+	},
+	POTATO: {
+		name: 'POTATO',
+		price: 20,
+		kcal: 5,
+	},
+	SEASONING: {
+		name: 'SEASONING',
+		price: 15,
+		kcal: 0,
+	},
+	MAYO: {
+		name: 'MAYO',
+		price: 20,
+		kcal: 5,
+	},
+};
 
-// Big burger
-console.group('Big burger');
-hamburger.addTopping().TOPPING_MAYO.updatePriceBig;
-console.log('Price with sauce: ' + hamburger.getPrice().SIZE_BIG);
-console.log('Callories with sauce: ' + hamburger.getCallories().SIZE_BIG);
-console.groupEnd();
+const smallBurger = new Burger(Burger.BURGER_SIZE.SMALL);
+smallBurger.addTopping(Burger.TOPPINGS.MAYO);
+smallBurger.addTopping(Burger.TOPPINGS.CHEESE);
+smallBurger.burgerInfo();
+
+const mediumBurger = new Burger(Burger.BURGER_SIZE.MEDIUM);
+mediumBurger.addTopping(Burger.TOPPINGS.MAYO);
+mediumBurger.addTopping(Burger.TOPPINGS.CHEESE);
+mediumBurger.addTopping(Burger.TOPPINGS.POTATO);
+mediumBurger.burgerInfo();
+
+const bigBurger = new Burger(Burger.BURGER_SIZE.BIG);
+bigBurger.addTopping(Burger.TOPPINGS.MAYO);
+bigBurger.addTopping(Burger.TOPPINGS.CHEESE);
+bigBurger.addTopping(Burger.TOPPINGS.POTATO);
+bigBurger.addTopping(Burger.TOPPINGS.SALAD);
+bigBurger.addTopping(Burger.TOPPINGS.SEASONING);
+bigBurger.burgerInfo();
